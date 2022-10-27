a79ad305935f853cd164c26b09ab1b28c8a095a1
vstsiganow <slavok-48@mail.ru>
2022-10-28 00:31:28 +0300

    Added Progress Header View for checking progress of completed tasks in section

--- Today/Models/Reminder.swift
+++ Today/Models/Reminder.swift
@@ -27,9 +27,9 @@ extension Array where Element == Reminder {
 #if DEBUG
 extension Reminder {
     static var sampleData = [
-            Reminder(title: "Submit reimbursement report", dueDate: Date().addingTimeInterval(800.0), notes: "Don't forget about taxi receipts"),
-            Reminder(title: "Code review", dueDate: Date().addingTimeInterval(14000.0), notes: "Check tech specs in shared folder", isComplete: true),
-            Reminder(title: "Pick up new contacts", dueDate: Date().addingTimeInterval(24000.0), notes: "Optometrist closes at 6:00PM"),
+            Reminder(title: "Submit reimbursement report", dueDate: Date().addingTimeInterval(-800.0), notes: "Don't forget about taxi receipts"),
+            Reminder(title: "Code review", dueDate: Date().addingTimeInterval(1000.0), notes: "Check tech specs in shared folder", isComplete: true),
+            Reminder(title: "Pick up new contacts", dueDate: Date().addingTimeInterval(-24000.0), notes: "Optometrist closes at 6:00PM"),
             Reminder(title: "Add notes to retrospective", dueDate: Date().addingTimeInterval(3200.0), notes: "Collaborate with project manager", isComplete: true),
             Reminder(title: "Interview new project manager candidate", dueDate: Date().addingTimeInterval(60000.0), notes: "Review portfolio"),
             Reminder(title: "Mock up onboarding experience", dueDate: Date().addingTimeInterval(72000.0), notes: "Think different"),
